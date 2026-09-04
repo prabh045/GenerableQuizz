156bc4874fc31db06dd2e09c18e12ca0974048fc
PrabhSodhi <prabhdeep045@gmail.com>
2026-09-04 21:09:34 +0530

    Added Quiz Generarator to generate quiz to a given topic

--- GenerableQuiz/Quiz.swift
+++ GenerableQuiz/Quiz.swift
@@ -5,8 +5,10 @@
 //  Created by Prabhdeep Singh on 04/09/26.
 //
 
-import Foundation
+import FoundationModels
 
+// Generable Macro so AI Model can give us back response of type Quiz
+@Generable
 struct Quiz {
     let questions: [String]
 }
---
+++ GenerableQuiz/QuizGenerator.swift
@@ -0,0 +1,34 @@
+//
+//  QuizGenerator.swift
+//  GenerableQuiz
+//
+//  Created by Prabhdeep Singh on 04/09/26.
+//
+
+import SwiftUI
+import FoundationModels
+
+@Observable
+class QuizGenerator {
+    let topic: String
+    var quiz: Quiz.PartiallyGenerated?
+    var isGenerating = false
+
+    init(topic: String) {
+        self.topic = topic
+    }
+    
+   // we give instructions to a LanguageModelSession to describe or contextualize the data you want back from it. This information acts like a prompt and helps refine the model’s responses.
+    func generateQuiz() {
+        let session = LanguageModelSession(instructions: "Create a quiz with the provided topic as the focus.")
+        let stream = session.streamResponse(to: topic, generating: Quiz.self)
+        
+        Task {
+            isGenerating = true
+            for try await partialData in stream {
+                self.quiz = partialData.content
+            }
+            isGenerating = false
+        }
+    }
+}

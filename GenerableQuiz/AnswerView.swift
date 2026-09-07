0ede241aebc78a15713f82b87a9409fe3e2c557a
PrabhSodhi <prabhdeep045@gmail.com>
2026-09-07 22:30:20 +0530

    Added correct and incorrect UI to answer View

--- GenerableQuiz/AnswerView.swift
+++ GenerableQuiz/AnswerView.swift
@@ -6,13 +6,62 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct AnswerView: View {
+    var displayAnswer: Answer.PartiallyGenerated
+    @Binding var selectedAnswer: Answer.PartiallyGenerated?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+//
+
+        } label: {
+            HStack {
+                Image(systemName: imageName)
+                Text(displayAnswer.text ?? "Generating...")
+                    .multilineTextAlignment(.leading)
+                    .foregroundStyle(.primary)
+                Spacer()
+            }
+        }
+        .buttonStyle(.bordered)
+        .foregroundStyle(tintColor)
+        .tint(tintColor)
     }
+    
+    private var tintColor: Color {
+        if let isAnswerCorrect {
+            return isAnswerCorrect ? .green : .red
+        } else {
+            return .secondary
+        }
+    }
+    
+    private var isAnswerCorrect: Bool? {
+        guard selectedAnswer?.id == displayAnswer.id else { return nil }
+        return displayAnswer.isCorrect
+    }
+    
+    private var imageName: String {
+        if let isAnswerCorrect {
+            return isAnswerCorrect ? "checkmark.circle.fill" : "xmark.circle.fill"
+        } else {
+            return "circle"
+        }
+    }
+    
+    
 }
 
 #Preview {
-    AnswerView()
+    @Previewable @State var correctAnswer = Optional(Answer.correctAnswer.asPartiallyGenerated())
+    @Previewable @State var incorrectAnswer = Optional(Answer.incorrectAnswer.asPartiallyGenerated())
+
+
+    VStack {
+        AnswerView(displayAnswer: correctAnswer!, selectedAnswer: $incorrectAnswer)
+        AnswerView(displayAnswer: correctAnswer!, selectedAnswer: $correctAnswer)
+        AnswerView(displayAnswer: incorrectAnswer!, selectedAnswer: $incorrectAnswer)
+    }
 }
